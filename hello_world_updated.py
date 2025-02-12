8ec69e8d6dd291e74d87200a2cd53a5d518a28ba
Enoch Oyeniyi <enoch.oyeniyi@stu.ocsb.ca>
2025-02-12 17:53:53 +0000

    I added a hello_world_style.py file

---
+++ hello_world_updated.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+# Created By: Enoch Oyeniyi
+# Date: February 12, 2025
+# This program displays Hello, World! with proper style
+#
+#
+def main():
+    print("Hello, World! with proper style")
+    print("I am testing my function here")
+
+
+if __name__ == "__main__":
+    main()
